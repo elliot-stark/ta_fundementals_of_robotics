9d46481842805f03d97719252c54287b485e0f73
elliot-stark <elliot_stark@student.uml.edu>
2026-09-08 19:13:03 -0400

    Changed display due to error

--- run-linux.sh
+++ run-linux.sh
@@ -3,7 +3,7 @@ container_name=${dirname}_dev
 
 docker run -it --rm \
   --name ${container_name} \
-  --env="DISPLAY=host.docker.internal:0" \
+  --env="DISPLAY=:0" \
   --env="QT_X11_NO_MITSHM=1" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   --env="XAUTHORITY=$XAUTH" \
