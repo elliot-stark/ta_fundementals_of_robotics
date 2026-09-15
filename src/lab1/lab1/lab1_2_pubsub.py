79e5d8787fb59fd6b6b4dae08f7a704f32648a4b
elliot-stark <elliot_stark@student.uml.edu>
2026-09-15 19:22:23 -0400

    Fixed distance formula

--- src/lab1/lab1/lab1_2_pubsub.py
+++ src/lab1/lab1/lab1_2_pubsub.py
@@ -2,23 +2,28 @@ import rclpy
 from std_msgs.msg import Float32
 from turtlesim.msg import Pose
 from rclpy.node import Node
+import math
 
 class Pose_subscriber(Node):
     def __init__(self):
         super().__init__('pose_sub')
         self.pos_publisher = self.create_publisher(Float32, 'distance_traveled', 10)
         self.pos_subscriber = self.create_subscription(Pose, '/turtle1/pose', self.pose_subscriber_callback, 10)
-        self.previous_location = None
+        self.previous_location_x = None
+        self.previous_location_y = None
         self.total_distance = 0.0
         
     def pose_subscriber_callback(self, msg):
         current_location = msg
 
-        if self.previous_location is not None:
-            dist_traveled = abs(current_location.x - self.previous_location)
+        if self.previous_location_x is not None and self.previous_location_y is not None:
+            x_dist = current_location.x - self.previous_location_x
+            y_dist = current_location.y - self.previous_location_y
+            dist_traveled = math.sqrt((x_dist**2 + y_dist**2))
             self.total_distance += dist_traveled
 
-        self.previous_location = current_location.x
+        self.previous_location_x = current_location.x
+        self.previous_location_y = current_location.y
 
         self.publish_distance()
 
